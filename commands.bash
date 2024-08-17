36264af5f18a68b127a10510905e454a9b0f08f5
Kavit Mehta <39302323+KavitDeepakMehta@users.noreply.github.com>
2024-08-17 21:28:06 +0530

    commands.bash

--- commands.bash
+++ commands.bash
@@ -1,8 +1,8 @@
-sudo su
-yum install python3 -y
-yum install python3-pip -y
-pip3 install Flask
-pip3 install boto3
-sudo nano app.py
-#Write your Python Code here and press (Ctrl + X and Enter) to exit from nano 
-python3 app.py
+sudo su #Super User Do Switch User
+yum install python3 -y #Installing Python3 in Amazon Linux 2
+yum install python3-pip -y #Installing Python3-pip in Amazon Linux 2
+pip3 install Flask #Installing Flask in Amazon Linux 2
+pip3 install boto3 #Installing boto3 in Amazon Linux 2
+sudo nano app.py #Creating a Python File to Write and Run the Code
+#Write your Python Code [app.py] here and press (Ctrl + X and Enter) to exit from nano 
+python3 app.py #To execute the script in your web server
